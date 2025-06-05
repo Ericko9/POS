bc63d20bdcd00125c941d994ead52399fe70a8ee
unknown <erickowicaksono9@gmail.com>
2025-06-05 13:08:37 +0700

    Simplify build configuration to fix loading issues

--- toko-management/vite.config.js
+++ toko-management/vite.config.js
@@ -4,22 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        format: 'es',
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    assetsDir: 'assets'
   }
 })
